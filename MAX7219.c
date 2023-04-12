8211de055c26e4b570634d0c28615bbc1c7cdf26
AnhTuan <113578763+TuanLBT@users.noreply.github.com>
2023-04-12 11:39:57 +0700

    Update MAX7219.c

--- MAX7219.c
+++ MAX7219.c
@@ -6,8 +6,16 @@
 #include "MAX7219.h"
 #define spi0   0
 uint8_t buf[2];
+uint8_t arrow_right[8] = {0x3C,0x3C,0x3C,0x3C,0xFF,0x7E,0x3C,0x18};
+uint8_t arrow_left[8] = {0x18,0x3C,0x7E,0xFF,0x3C,0x3C,0x3C,0x3C};
+uint8_t arrow_front[8] = {0x08,0x0C,0xFE,0xFF,0xFF,0xFE,0x0C,0x08};
+uint8_t arrow_back[8] = {0x10,0x30,0x70,0xFF,0xFF,0x70,0x30,0x10};
 
-//display float fuction (copy hàm của thầy)
+//display number without decode mode
+uint8_t digit[10] = {0X7E,0x30,0x6D,0x79,0x33,0x5B,0x5F,0x70,0x7F,0x7B};
+
+
+//display float fuction
 void display_float(float num, uint8_t dec){
     int16_t integerPart = num;
     int16_t fractionalPart = (num - integerPart) * pow(10,dec);
@@ -32,14 +40,14 @@ void display_float(float num, uint8_t dec){
 }
 
 //****************************************************************7 segments led**********************************************************************
-//Init function (hàm khởi tạo led 7 đoạn)
+//HAM INIT
 int j;
 void Init_7segments(void){ 
-    // set decode mode: 0x09 FF (địa chỉ thanh ghi là 0x09)
+    // set decode mode: 0x09FF
     sendData(0x09,0xFF);
-    // set intensity: 0x0A 09
+    // set intensity: 0x0A09
     sendData(0x0A, 9);
-    // scan limit: 0x0B  hiển thị cùng lúc 8 ô led 7 đoạn table 8.  Scan-Limit Register Format (Address (Hex) = XB)
+    // scan limit: 0x0B  (07) Display digits 0 1 2 3 4 5 6 7 table 8.  Scan-Limit Register Format (Address (Hex) = XB)
     sendData(0x0B, 7);
     // no shutdown, turn off display test
     sendData(0x0C, 1);
@@ -54,7 +62,7 @@ void sendData(uint8_t address, uint8_t data){
     wiringPiSPIDataRW(spi0, buf, 2);
 }
 
-//gui 1 chuỗi data va hienthi lên led 
+//gui data va hienthi
 void sendData_SHOW(uint8_t data[], uint8_t datasize)
 {
     for (int i=0; i<datasize;i++)
@@ -62,8 +70,33 @@ void sendData_SHOW(uint8_t data[], uint8_t datasize)
         sendData(i+1, data[datasize - 1 -i]);
     }
 }
+//hien thi 2 so len
+void display_2values(float alphaX, float alphaY)
+{
+    int i_alphaX = alphaX*10; 
+    int i_alphaY = alphaY*10; //hien thi so thap phan dau tien sau dau phay
+    i_alphaX = fabs(i_alphaX);
+    i_alphaY = fabs(i_alphaY);
+    //lay tung chu so
+    uint8_t thapphan_alphaX = i_alphaX%10;
+    uint8_t donvi_alphaX = (i_alphaX%100)/10;
+    uint8_t chuc_alphaX = i_alphaX/100;
 
-//Hàm hiển thị số (số nhiều hàng đơn vị)
+    uint8_t thapphan_alphaY = i_alphaY%10;
+    uint8_t donvi_alphaY = (i_alphaY%100)/10;
+    uint8_t chuc_alphaY = i_alphaY/100;
+
+    sendData(5,0x63);
+    sendData(6,digit[thapphan_alphaX]);
+    sendData(7,digit[donvi_alphaX] | 0x80);
+    sendData(8,digit[chuc_alphaX]);
+
+    sendData(1,0x63);
+    sendData(2,digit[thapphan_alphaY]);
+    sendData(3,digit[donvi_alphaY] | 0x80);
+    sendData(4,digit[chuc_alphaY]);
+}
+//HAM HHIEN THI SO
 void display_number(uint32_t num){ 
     // count the number of digits
     uint8_t count=1;
@@ -163,9 +196,9 @@ void shift_data(uint8_t data[], uint8_t datasize , uint8_t solandich, uint8_t di
 
 //****************************************************************8x8 matrix**********************************************************************
 
-//Khởi tạo led ma trận 8x8 
+
 void Init_8x8matrix(void){ 
-    // set decode mode: 0x09FF
+    // off decode mode: 0x09FF
     sendData(0x09,0x00);
     // set intensity: 0x0A09
     sendData(0x0A, 9);
@@ -176,10 +209,55 @@ void Init_8x8matrix(void){
     sendData(0x0F, 0);
 }
 
-//Hàm clear hết những gì có trên led 8x8
 void clear_8x8(void)
 {
     for(int i=0; i<8;i++){
         sendData(i+1,0);
+    }
+}
+
+void arrow(int dir)
+{
+    switch (dir)
+    {
+        case 1:
+            clear_8x8();
+            delay(100);
+            for (int i =0; i<8;i++)
+            {
+            sendData(i+1,arrow_front[i]);
+            }
+            delay(100); 
+            break;
+
+        case 2:
+            clear_8x8();
+            delay(100);
+            for (int i =0; i<8;i++)
+            {
+            sendData(i+1,arrow_back[i]);
+            }
+            delay(100); 
+            break;
+
+        case 3:
+            clear_8x8();
+            delay(100);
+            for (int i =0; i<8;i++)
+            {
+            sendData(i+1,arrow_right[i]);
+            }
+            delay(100); 
+            break;
+
+        case 4:
+            clear_8x8();
+            delay(100);
+            for (int i =0; i<8;i++)
+            {
+            sendData(i+1,arrow_left[i]);
+            }
+            delay(100); 
+            break;
     }
 }
