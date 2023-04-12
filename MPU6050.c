ec7648a7e5267c953715f4ef98103857d81ad21f
AnhTuan <113578763+TuanLBT@users.noreply.github.com>
2023-04-12 11:38:59 +0700

    Update MPU6050.c

--- MPU6050.c
+++ MPU6050.c
@@ -24,7 +24,7 @@ void check_connect(int mpu)
 }
 
 
-//read value from resgister of sensor (đọc giá trị của thanh ghi)
+//read value from resgister of sensor
 int16_t readSensor(int mpu,uint8_t address){
     uint8_t high,low;
     int16_t value;
@@ -35,8 +35,7 @@ int16_t readSensor(int mpu,uint8_t address){
     
 }
 
-//read temperature (đọc nhiệt độ của thanh ghi nhiệt độ)
-
+//read temperature
 float temperature(int mpu) //numbers of digits display after demical dot
 {
     float temp; int16_t value;
@@ -45,7 +44,25 @@ float temperature(int mpu) //numbers of digits display after demical dot
     return temp;
 }
 //read accelerator
+float roll(int mpu)
+{
+    float AccX,AccY,AccZ; float roll;
+    AccX = readSensor(mpu,0x3B) / 16384.0;
+    AccY = readSensor(mpu,0x3D) / 16384.0;
+    AccZ = readSensor(mpu,0x3F) / 16384.0;
 
+    roll = (atan((AccY) / sqrt(pow((AccX), 2) + pow((AccZ), 2))) * 180 / M_PI);
+    return roll;
+}
+float pitch(int mpu)
+{
+    float AccX,AccY,AccZ; float pitch;
+    AccX = readSensor(mpu,0x3B) / 16384.0;
+    AccY = readSensor(mpu,0x3D) / 16384.0;
+    AccZ = readSensor(mpu,0x3F) / 16384.0;
+    pitch = (atan(-1 * (AccX) / sqrt(pow((AccY), 2) + pow((AccZ), 2))) * 180 / M_PI);
+    return pitch;
+}
 
 // int main(void){
 //     //set up i2c 
